31548b23c6d339ff7d66011c32c3bbd7a0f9369b
Victor Brito <victorsebrito@gmail.com>
2017-03-11 19:08:00 -0300

    API

---
+++ TeoriaDosGrafos/APIResources.cs
@@ -0,0 +1,44 @@
+﻿using Grapevine;
+using Grapevine.Server;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+
+namespace TeoriaDosGrafos
+{
+    public sealed class APIResources : RESTResource
+    {
+        [RESTRoute (Method = HttpMethod.GET, PathInfo = "")]
+        public void HandleAllGetRequests(HttpListenerContext context)
+        {
+            this.SendTextResponse(context, "GET is a success!");
+        }
+
+        [RESTRoute (Method = HttpMethod.POST, PathInfo = @"^/api/grafo/?$")]
+        public void NewGraph(HttpListenerContext context)
+        {
+            Dictionary<string, string> loArgs = GetDictionaryFromContext(context);
+            
+            if (loArgs.ContainsKey("file"))
+            {
+
+            }
+            else
+            {
+                MainClass.graph = new Graph();
+            }
+        }
+
+        public Dictionary<string, string> GetDictionaryFromContext(HttpListenerContext context)
+        {
+            string body = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding).ReadToEnd();
+            NameValueCollection nvc = HttpUtility.ParseQueryString(body);
+            return nvc.AllKeys.ToDictionary(k => k, k => nvc[k]);
+        }
+    }
+}
--- TeoriaDosGrafos/Program.cs
+++ TeoriaDosGrafos/Program.cs
@@ -1,20 +1,35 @@
 ﻿using System;
+using Grapevine.Server;
+using System.Threading;
 
 namespace TeoriaDosGrafos 
 {
     class MainClass
 	{
+        public static Graph graph { get; set; }
+
 		public static void Main(string[] args)
 		{
-		    Graph graph = new Graph();
-		    do
-		    {
-		        var vertice = graph.LerVertice();
-		        var source = graph.LerArestaSource(vertice);
-		        var target = graph.LerArestaTarget(vertice);
-		        var edgeWeight = graph.EdgeWeight();
+            var server = new RESTServer();
+            server.Start();
+
+            while (server.IsListening)
+            {
+                Thread.Sleep(300);
+            }
+
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+
+      //      Graph graph = new Graph();
+		    //do
+		    //{
+		    //    var vertice = graph.LerVertice();
+		    //    var source = graph.LerArestaSource(vertice);
+		    //    var target = graph.LerArestaTarget(vertice);
+		    //    var edgeWeight = graph.EdgeWeight();
 		       
-		    } while (Console.ReadLine() != "");
+		    //} while (Console.ReadLine() != "");
 
 		}
         
