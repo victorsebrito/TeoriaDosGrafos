686b7c5dab3d42c2bcd3dcc7a28c765fc98deef1
byxande <alexandrepbsts@gmail.com>
2017-03-11 01:10:14 -0300

    Add files via upload

---
+++ TeoriaDosGrafos-master/TeoriaDosGrafos/Edge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace TeoriaDosGrafos
+{
+	public class Edge
+	{
+		public Edge()
+		{
+		}
+        public int id { get; set; }
+        public string Source { get; set; }
+        public string Target { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ TeoriaDosGrafos-master/TeoriaDosGrafos/Vertice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TeoriaDosGrafos
+{
+	public class Vertex
+	{
+		public Vertex()
+		{
+		}
+
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
