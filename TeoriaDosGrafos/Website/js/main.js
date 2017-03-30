104affa73141296652c0775f8af15b53f3513d6f
Tauan Matos <tauan.matos164@gmail.com>
2017-03-30 20:48:23 -0300

    D3 "Funcionando"

--- TeoriaDosGrafos/Website/js/main.js
+++ TeoriaDosGrafos/Website/js/main.js
@@ -198,3 +198,77 @@ var arestasEntreVertices = function () {
     api.aresta.lista(queryString);
     return false;
 }
+
+
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function (d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+d3.json("Grafos.json", function (error, graph) {
+    if (error) throw error;
+
+    var link = svg.append("g")
+        .attr("class", "links")
+      .selectAll("line")
+      .data(graph.Arestas)
+      .enter().append("line")
+        .attr("stroke-width", function (d) { return Math.sqrt(d.peso); });
+
+    var node = svg.append("g")
+        .attr("class", "nodes")
+      .selectAll("circle")
+      .data(graph.Vertices)
+      .enter().append("circle")
+        .attr("r", 5)
+        .attr("fill", function (d) { return color(d.id); })
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    node.append("title")
+        .text(function (d) { return d.nome; });
+
+    simulation
+        .nodes(graph.Vertices)
+        .on("tick", ticked);
+
+    simulation.force("link")
+        .links(graph.Arestas);
+
+    function ticked() {
+        link
+            .attr("x1", function (d) { return d.source.x; })
+            .attr("y1", function (d) { return d.source.y; })
+            .attr("x2", function (d) { return d.target.x; })
+            .attr("y2", function (d) { return d.target.y; });
+
+        node
+            .attr("cx", function (d) { return d.x; })
+            .attr("cy", function (d) { return d.y; });
+    }
+});
+
+function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+}
+
+function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+}
